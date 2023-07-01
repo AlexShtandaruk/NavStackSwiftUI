56e5d9cfce597405687cc6a5fe9c547cf156babd
AlexShtandaruk <125973696+AlexShtandaruk@users.noreply.github.com>
2023-07-01 18:38:32 +0300

    1.0.4

--- Sources/NavigationStack/Screen.swift
+++ Sources/NavigationStack/Screen.swift
@@ -3,10 +3,10 @@ import SwiftUI
 @available(iOS 13.0, *)
 public struct Screen: Identifiable, Equatable {
     
-    let id = UUID().uuidString
-    let view: AnyView
+    public let id = UUID().uuidString
+    public let view: AnyView
     
-    static func == (lhs: Screen, rhs: Screen) -> Bool {
+    public static func == (lhs: Screen, rhs: Screen) -> Bool {
         return lhs.id ==  rhs.id
     }
 }
